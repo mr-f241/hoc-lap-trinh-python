9c46022d7930014a711e7c40aa91aaca30d0c424
mr-f241 <laokay037@gmail.com>
2025-05-06 07:13:04 +0700

    init commit

---
+++ ltcb1.py
@@ -0,0 +1,16 @@
+''' Toán tử and or not
+print(0 and 2)
+trả về true nếu nó khác 0 và in ra cái lón hơn 
+còn false nếu nó = 0 và in ra 0
+print(1 or 9999)
+or thì ngược lại với and là in nhỏ hơn và vói 0 thì lại in ra số khác 0
+'''
+s = "anh yêu em"
+print(s.capitalize())
+print(s.title())
+print(s.lower())
+print(s.upper())
+
+age = 3 #Int
+# I am 3
+print(f"I am {age}")
